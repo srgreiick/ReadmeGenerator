e01455133d1f36d8e78bb95bb85d86e95937b208
Anthrax <samgreilick@gmail.com>
2023-04-09 15:40:41 -0600

    inquirer added and a os. function to remove the old readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ReadmeGenerator
-This is going to be a python readme generator as an alternative to my js/node one from class
+asdf
+  a
+  ASdf
--- generator.py
+++ generator.py
@@ -1,2 +1,28 @@
 print("---------------------Good morning, let's make a readme---------------------")
-input()
+import os
+import inquirer
+
+
+questions = [
+    inquirer.Text("user", message="Please enter your github username", validate=lambda _, x: x != ".")
+]
+answers = inquirer.prompt(questions)
+
+
+print(answers)
+print(answers["user"])
+
+readmeContent = (
+  f'''asdf
+  {answers["user"]}
+  ASdf'''
+)
+print(readmeContent)
+
+if os.path.exists("README.md"):
+  os.remove("README.md")
+else:
+  print("The file does not exist")
+readme = open("README.md", "x")
+
+readme.write(f"{readmeContent}")
